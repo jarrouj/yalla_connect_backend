457ee1d1a6236eab86846570c6dcea7aac85a338
= <georgesjarrouj15@gmail.com>
2025-07-31 19:54:27 +0300

    small fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,6 +25,9 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+                        $table->string('verification_code')->nullable();
+            $table->string('reset_code')->nullable();
+            $table->string('fcm_token')->nullable();
             $table->timestamps();
         });
 
