2dc65131f6add114c89057755308b7c7beb4650f
jorj <georgesjarrouj15@gmail.com>
2025-08-10 09:59:18 +0300

    small fixes

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,7 +5,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="" href="{{ url('/') }}">
-            <img src="/logo/codergize.svg" width="250px" class="d-block mx-auto p-3" alt="main_logo">
+            <img src="{{ asset('images/logo/yalla_connect.png') }}" width="130px" height="130px" class="d-block mx-auto p-3" alt="main_logo">
         </a>
 
     </div>
