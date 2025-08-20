d6216a47072eb609dfd53bbdb2ed6d1b64fa0729
jorj <georgesjarrouj15@gmail.com>
2025-08-20 18:33:50 +0300

    t

--- database/migrations/2025_07_29_173803_add_subcategory_to_products_table.php
+++ database/migrations/2025_07_29_173803_add_subcategory_to_products_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-                        $table->foreignId('subcategory_id')->nullable()->after('category_id')->constrained('subcategories')->onDelete('cascade');
-
+            $table->foreignId('subcategory_id')->nullable()->after('category_id')->constrained('subcategories')->onDelete('cascade');
         });
     }
 
@@ -23,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-              $table->dropForeign(['subcategory_id']);
+            $table->dropForeign(['subcategory_id']);
             $table->dropColumn(['subcategory_id']);
         });
     }
